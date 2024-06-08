c846834434968ca3119609cd84400bc59eb11b45
sahildesai13 <sahildesai7338@gmail.com>
2024-06-08 12:03:25 +0530

    system mode issue solved

--- src/app/componets/ToggleSwitch/ToggleSwitch.jsx
+++ src/app/componets/ToggleSwitch/ToggleSwitch.jsx
@@ -1,57 +1,21 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import { PiSunDuotone } from 'react-icons/pi';
+"use client";
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 import { RxMoon } from 'react-icons/rx';
-import { useTheme } from 'next-themes';
+import { CgDarkMode } from "react-icons/cg";
+import { PiSunDuotone } from "react-icons/pi";
 
-function ToggleSwitch() {
-    const [mounted, setMounted] = useState(false);
-    const { theme, setTheme } = useTheme("dark");
-    const [DarkBtn, setDarkBtn] = useState(false);
+const ToggleSwitch = () => {
+    const [mounted, setMounted] = useState(false)
     const [isOpen, setIsOpen] = useState(false);
+    const { theme, setTheme } = useTheme();
 
     useEffect(() => {
         setMounted(true)
     }, [])
 
-    if (!mounted) return null
-
-    // const setMode = (mode) => {
-    //     if (mode == 'system') {
-    //         window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    //             setTheme(event.matches ? 'dark' : 'light');
-    //             setDarkBtn(theme);
-    //         });
-    //     } else {
-    //         setTheme(mode);
-    //     }
-    //     toggleDropdown();
-    // }
-
-    const setMode = (mode) => {
-        if (mode === 'system') {
-            const systemDarkModeListener = window.matchMedia('(prefers-color-scheme: dark)');
-
-            const handleSystemDarkModeChange = (event) => {
-                setTheme(event.matches ? 'dark' : 'light');
-                setDarkBtn(event.matches ? 'dark' : 'light');
-            };
-
-            systemDarkModeListener.addEventListener('change', handleSystemDarkModeChange);
-
-            // Set the initial theme based on system preference
-            setTheme(systemDarkModeListener.matches ? 'dark' : 'light');
-            setDarkBtn(systemDarkModeListener.matches ? 'dark' : 'light');
-
-            // Clean up the event listener when the component unmounts
-            return () => {
-                systemDarkModeListener.removeEventListener('change', handleSystemDarkModeChange);
-            };
-        } else {
-            setTheme(mode);
-            setDarkBtn(mode === 'dark' ? 'dark' : 'light');
-        }
-        toggleDropdown();
+    if (!mounted) {
+        return null
     }
 
     const toggleDropdown = () => {
@@ -67,24 +31,28 @@ function ToggleSwitch() {
     window.addEventListener('mousedown', outsideToggle);
 
     return (
-        <div className='container flex px-0 duration-300'>
-            <div className="relative dropdown-container">
-                <button onClick={toggleDropdown} className="inline-flex items-center justify-center w-[40px] py-1 text-xl dark:text-gray-200 bg-transparent text-white dark:bg-dark-black dark:hover:bg-hover-black rounded-md duration-300 " >
-                    {theme == 'system' ? DarkBtn == "dark" ? <RxMoon /> : <PiSunDuotone className='text-black' /> : theme == 'dark' ? <RxMoon /> : <PiSunDuotone className='text-black' />}
-                </button>
-                <div className={`absolute right-0 z-10 mt-2 w-[100px] bg-white text-black shadow-lg dark:shadow dark:shadow-card-border dark:bg-dark-black rounded-md transition-all origin-top-right duration-300 ${isOpen ? "scale-100" : "scale-0"} `}>
-                    <ul className="text-center p-1">
-                        {["dark", "light", "system"].map((ele, ind) => {
-                            return (
-                                <li key={ind} className='hover:bg-slate-100 dark:hover:bg-hover-black rounded-sm duration-300 dark:text-white'>
-                                    <button onMouseDown={() => { setMode(ele) }} className="w-full outline-none text-start px-1 py-1 capitalize">{ele}</button>
-                                </li>
-                            )
-                        })}
-                    </ul>
+        <div className='text-black dark:text-white'>
+            {/* ------------------------------------------------ */}
+            <div className='container flex px-0 duration-300'>
+                <div className="relative dropdown-container">
+                    <button onClick={toggleDropdown} className="inline-flex items-center justify-center w-[35px] h-[35px] py-1 text-xl dark:text-gray-200 bg-transparent text-white dark:bg-dark-black dark:hover:bg-hover-black rounded-md duration-300 " >
+                        {theme == 'system' ? <CgDarkMode className='text-gray-600 dark:text-white' /> : theme === 'dark' ? <RxMoon /> : <PiSunDuotone className='text-black' />}
+                    </button>
+                    <div className={`absolute right-0 z-10 mt-2 w-[100px] bg-white text-black shadow-lg dark:shadow dark:shadow-card-border dark:bg-dark-black rounded-md transition-all origin-top-right duration-300 ${isOpen ? "scale-100" : "scale-0"} `}>
+                        <ul className="text-center p-1">
+                            {["dark", "light", "system"].map((ele, ind) => {
+                                return (
+                                    <li key={ind} className='hover:bg-slate-100 dark:hover:bg-hover-black rounded-sm duration-300 dark:text-white'>
+                                        <button onMouseDown={() => { setTheme(ele) }} className="w-full outline-none text-start px-1 py-1 capitalize">{ele}</button>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
